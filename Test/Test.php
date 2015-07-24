f59b50d51b6fd7cb60f30e8173994d85e59ad45b
Ronen-dev <ronenn.ma@gmail.com>
2015-07-24 11:26:18 +0200

    Envoi d'un fichier de test

---
+++ Test/Test.php
@@ -0,0 +1,8 @@
+Fichier de test
+
+
+Comment implémenter une socket en C ?
+
+- Faut la créer avec la fonction socket, tu las connecte avec la fonction connect().
+
+L'avantage d'une lite chainée sur un tableau ?
