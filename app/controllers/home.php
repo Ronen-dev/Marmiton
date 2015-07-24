01a11d7a95d899763454f9892f859338eade8f01
Ronen-dev <ronenn.ma@gmail.com>
2015-07-24 16:38:01 +0200

    Ajout de nouveau fichiers

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -1,9 +1,12 @@
 <?php
 
-class home extends Controller
+class Home extends Controller
 {
     public function index($name = '')
     {
-        echo $name;
+        $user = $this->model('User');
+        $user->$name = $name;
+
+        $this->view('home/index');
     }
 }
